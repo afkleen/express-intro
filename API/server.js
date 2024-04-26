b0e72b7c066883a98d7b3ba1c8dfff2caf50c2b4
afkleen <vilhelm.melkeraaenafkleen@skola.taby.se>
2024-04-26 12:31:29 +0200

    api

---
+++ API/server.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2/promise");
+const jwt = require("jsonwebtoken");
+
+// parse application/json, för att hantera att man POSTar med JSON
+const bodyParser = require("body-parser");
+
+// Inställningar av servern.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+async function getDBConnnection() {
+  // Här skapas ett databaskopplings-objekt med inställningar för att ansluta till servern och databasen.
+  return await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "databas",
+  });
+}
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Doumentation EXEMPEL</h1>
+  <ul><li> GET /users</li></ul>`);
+});
+
+app.get("/users", async function (req, res) {
+  let connection = await getDBConnnection();
+  let sql = `SELECT * from users`;
+  let [results] = await connection.execute(sql);
+
+  //res.json() skickar resultat som JSON till klienten
+  res.json(results);
+});
+
+app.post("/users", async function (req, res) {
+  //req.body innehåller det postade datat
+  console.log(req.body);
+
+  let connection = await getDBConnnection();
+  let sql = `INSERT INTO users (username, name, password)
+     VALUES (?, ?, ?)`;
+
+  let [results] = await connection.execute(sql, [
+    req.body.username,
+    req.body.name,
+    req.body.password,
+  ]);
+
+  //results innehåller metadata om vad som skapades i databasen
+  console.log(results);
+  res.json(results);
+});
+
+/*
+  app.post() hanterar en http request med POST-metoden.
+*/
+app.post("/users", function (req, res) {
+  // Data som postats till routen ligger i body-attributet på request-objektet.
+  console.log(req.body);
+
+  // POST ska skapa något så här kommer det behövas en INSERT
+  let sql = `INSERT INTO ...`;
+});
+
+app.post("/users/login", async function (req, res) {
+  //Denna kod skapar en token att returnera till anroparen
+  let connection = await getDBConnnection();
+  const salt = await bcrypt.genSalt(10); // genererar ett salt till hashning
+  const hashedPassword = await bcrypt.hash(req.body.password, salt); //hashar lösenordet
+
+  let sql = `SELECT users
+  WHERE username = ?, password = ?, id = ?`;
+
+  // let [results] = await connection.execute(sql, [
+  //   req.body.username,
+  //   hashedPassword,
+  //   req.params.id,
+  // ]);
+
+  // if(req.body.username){};
+
+  let payload = {
+    sub: req.body.id, // sub är obligatorisk
+    name: req.body.username,
+  };
+  let token = jwt.sign(payload, "secrets");
+  res.send(token);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
+
+app.put("/users/:id", async function (req, res) {
+  //kod här för att hantera anrop…
+  let sql = `UPDATE users
+      SET username = ?, name = ?, password = ?, email = ?
+      WHERE id = ?`;
+  let connection = await getDBConnnection();
+  const salt = await bcrypt.genSalt(10); // genererar ett salt till hashning
+  const hashedPassword = await bcrypt.hash(req.body.password, salt); //hashar lösenordet
+
+  let [results] = await connection.execute(sql, [
+    req.body.username,
+    req.body.name,
+    hashedPassword,
+    req.body.email,
+    req.params.id,
+  ]);
+  //kod här för att returnera data
+  console.log(results);
+  res.json(results);
+});
+
+const bcrypt = require("bcrypt");
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  const salt = await bcrypt.genSalt(10); // genererar ett salt till hashning
+  const hashedPassword = await bcrypt.hash(password, salt); //hashar lösenordet
+};
--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080; // Du kan välja en annan port om du vill
 
+var mysql = require("mysql2");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "databas",
+});
+// Route för roten
 app.get("/", (req, res) => {
-  res.send("Hello antonanoansnda!");
+  res.send("Hej från roten!");
 });
 
+// Route för /greet
+app.get("/users", (req, res) => {
+  //Paketet mysql är installerat med "npm install mysql"
+
+  // Här skapas ett databaskopplings-objekt med inställningar för att ansluta till servern och databasen.
+
+  // Här testas kopplingen till databasen.
+  connection.connect(function (error) {
+    // Den här funktionen är den callback som körs då uppkopplingen gjorts.
+    if (error) throw error;
+    console.log("Connection to DB created successfully.");
+  });
+
+  connection.query("SELECT * from users", function (error, results, fields) {
+    if (error) throw error;
+    console.log(fields);
+
+    // Man kan loopa igenom fields och skriva ut namnen på kolumnerna.
+    fields.forEach((field) => {
+      console.log(field.name);
+    });
+
+    // inparametern results innehåller resultatet av din query. Om det är en SELECT är det en array med objekt, ett objekt för varje rad.
+    console.log(results);
+    results.forEach((element) => {});
+  });
+
+  const users = req.query.users;
+});
+
+// Starta servern
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Servern körs på http://localhost:${port}`);
 });
